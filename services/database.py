eb34e94f9bc62e1c84c942753c2ca74246c3efe1
Artem Shurshilov <shurshilov.a@yandex.ru>
2024-05-13 21:14:48 +0300

    [FIX] native asterisk cdr work

--- services/database.py
+++ services/database.py
@@ -5,6 +5,29 @@ from schemas.config_schema import Config
 
 
 class DatabaseStrategy:
+    """CDR native asterisk table
+    CREATE TABLE cdr (
+        calldate datetime NOT NULL default '0000-00-00 00:00:00',
+        clid varchar(80) NOT NULL default '',
+        src varchar(80) NOT NULL default '',
+        dst varchar(80) NOT NULL default '',
+        dcontext varchar(80) NOT NULL default '',
+        channel varchar(80) NOT NULL default '',
+        dstchannel varchar(80) NOT NULL default '',
+        lastapp varchar(80) NOT NULL default '',
+        lastdata varchar(80) NOT NULL default '',
+        duration int(11) NOT NULL default '0',
+        billsec int(11) NOT NULL default '0',
+        disposition varchar(45) NOT NULL default '',
+        amaflags int(11) NOT NULL default '0',
+        accountcode varchar(20) NOT NULL default '',
+        uniqueid varchar(32) NOT NULL default '',
+        userfield varchar(255) NOT NULL default '',
+        did varchar(255) NOT NULL default '',
+        recordingfile varchar(255) NOT NULL default ''
+    )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+    """
+
     def __init__(self, config: Config) -> None:
         self.config = config
 
@@ -17,7 +40,7 @@ class SqliteStrategy(DatabaseStrategy):
         async with aiosqlite.connect(self.config.db_host) as db:
             db.row_factory = aiosqlite.Row
             async with db.execute(
-                f"SELECT * FROM {self.config.db_table_cdr_name} where start >= %s and start <= %s limit 100000;",
+                f"SELECT * FROM {self.config.db_table_cdr_name} where calldate >= %s and calldate <= %s limit 100000;",
                 [start_date, end_date],
             ) as cursor:
                 rows = await cursor.fetchall()
@@ -38,7 +61,7 @@ class MysqlStrategy(DatabaseStrategy):
 
         cur = await conn.cursor()
         await cur.execute(
-            f"SELECT * FROM {self.config.db_table_cdr_name} where start >= %s and start <= %s limit 100000;",
+            f"SELECT * FROM {self.config.db_table_cdr_name} where calldate >= %s and calldate <= %s limit 100000;",
             (start_date, end_date),
         )
         rows = await cur.fetchall()
@@ -55,7 +78,7 @@ class PostgresqlStrategy(DatabaseStrategy):
         conn = await aiopg.connect(dsn)
         cur = await conn.cursor()
         await cur.execute(
-            f"SELECT * FROM {self.config.db_table_cdr_name} where start >= %s and start <= %s limit 100000;",
+            f"SELECT * FROM {self.config.db_table_cdr_name} where calldate >= %s and calldate <= %s limit 100000;",
             (start_date, end_date),
         )
         rows = await cur.fetchall()
